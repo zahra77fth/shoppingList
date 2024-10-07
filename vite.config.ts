682cef21b7d3eaed53f00deb4525905ab241ede0
Afagh <afagh@Afaghs-MacBook-Pro.local>
2024-10-07 04:34:16 +0330

    feat: Deploy GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/shoppingList/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
